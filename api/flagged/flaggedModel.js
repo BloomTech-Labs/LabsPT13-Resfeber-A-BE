b113412a44cdc8d9e36e421da154a5a4e4b79ccf
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-11-11 22:10:45 -0500

    missed one file

--- api/flagged/flaggedModel.js
+++ api/flagged/flaggedModel.js
@@ -1,20 +1,23 @@
 const db = require('../../data/db-config');
 
 const getFlagged = async (userId) => {
-  return await db('flagged_destinations').where({ 'user': userId});
+  return await db('flagged_destinations').where({ user: userId });
 };
 
 const addFlagged = (userId, name) => {
-  return db('flagged_destinations').insert({"user": userId, "destination_name": name})
-  .returning("id");
+  return db('flagged_destinations')
+    .insert({ user: userId, destination_name: name })
+    .returning('id');
 };
 
 const removeFlagged = async (userId, destination_name) => {
-  return db('flagged_destinations').where({'user': userId, 'destination_name': destination_name}).delete()
+  return db('flagged_destinations')
+    .where({ user: userId, destination_name: destination_name })
+    .delete();
 };
 
 module.exports = {
   getFlagged,
   addFlagged,
-  removeFlagged
+  removeFlagged,
 };
