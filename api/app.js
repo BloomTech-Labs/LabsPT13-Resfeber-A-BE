7238209f4ec570400a0aa3747829bbfda2d2fb4b
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-11-12 22:27:22 -0500

    cors preflight again.2

--- api/app.js
+++ api/app.js
@@ -47,9 +47,16 @@ app.options('*', cors());
 app.use(
   cors({
     origin: [/localhost/, 'http://localhost:3000'],
-    credentials: true,
   })
 );
+app.use('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
