12af5e7e40d2725e4935083e1c5a61c3db6333c7
burner <lol@mail.com>
2020-11-16 23:08:44 -0500

    removed dead code

--- api/app.js
+++ api/app.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const jsdocConfig = require('../config/jsdoc');
 const dotenv = require('dotenv');
 const config_result = dotenv.config();
 const authRequired = require('./middleware/authRequired');
@@ -14,11 +11,6 @@ const authRequired = require('./middleware/authRequired');
 if (process.env.NODE_ENV != 'production' && config_result.error) {
   throw config_result.error;
 }
-
-const swaggerSpec = swaggerJSDoc(jsdocConfig);
-const swaggerUIOptions = {
-  explorer: true,
-};
 
 //###[  Routers ]###
 const indexRouter = require('./index/indexRouter');
@@ -35,11 +27,6 @@ process.on('unhandledRejection', (reason, p) => {
   // application specific logging, throwing an error, or other logic here
 });
 // docs would need to be built and committed
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, swaggerUIOptions)
-);
 
 app.use(helmet());
 app.use(express.json());
