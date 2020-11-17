26586cca29411bafa78779df651ef41a8dfffc03
burner <lol@mail.com>
2020-11-16 21:18:48 -0500

    cors attempt #582193

--- api/app.js
+++ api/app.js
@@ -50,6 +50,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('*', authRequired);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else next();
+});
+
 // application routes
 app.use('/', indexRouter);
 app.use(['/profile', '/profiles'], profileRouter);
