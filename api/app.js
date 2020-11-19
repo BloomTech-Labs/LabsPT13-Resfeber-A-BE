80cde59b940cb8757e122f9d3da57853278ffd0d
burner <lol@mail.com>
2020-11-18 23:05:29 -0500

    cors credentials

--- api/app.js
+++ api/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.options('*', cors());
 
 // application routes
