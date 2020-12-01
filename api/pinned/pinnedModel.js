64207612d7ea4b26c1d9f4e959da89f5ec309bf4
jgreenweb <57242175+jgreenweb@users.noreply.github.com>
2020-12-01 00:39:57 -0500

    -flagged table and routes, +pinned teleport_id support, +events flagged field

--- api/pinned/pinnedModel.js
+++ api/pinned/pinnedModel.js
@@ -4,15 +4,17 @@ const getPinned = async (userId) => {
   return await db('pinned_destinations').where({ user: userId });
 };
 
-const addPinned = (userId, name) => {
+const addPinned = (pinned) => {
+  const id = pinned.id;
+  delete pinned.id;
   return db('pinned_destinations')
-    .insert({ user: userId, destination_name: name })
+    .insert({ user: id, ...pinned })
     .returning('id');
 };
 
-const removePinned = async (userId, destination_name) => {
+const removePinned = async (userId, teleport_id) => {
   return db('pinned_destinations')
-    .where({ user: userId, destination_name: destination_name })
+    .where({ user: userId, teleport_id: teleport_id })
     .delete();
 };
 
